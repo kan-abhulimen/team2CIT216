6baae8967980b685abf8f3d3b62cd189625a672e
kan-abhulimen <ehi12001@byui.edu>
2014-05-19 14:58:16 -0600

    New project

---
+++ Noway/1st_presentation.php
@@ -0,0 +1,33 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="UTF-8">
+        <title>Welcome</title>
+        <style>
+body
+{
+background-color:#FFFF99;
+}
+</style>
+    </head>
+    <body>
+        <h2>If Statement</h2>
+        <img border="0" src="/images/if_statement.PNG" alt="If Statement" width="600" height="228">
+        <br>
+        <?php
+\date_default_timezone_get("America/Denver");
+    $t=  date("h:m:s:am");
+            
+    if ($t < "11:59:59:am") {
+        echo"Have a good day!";
+    }
+   
+?> 
+        <br>
+        <a href="http://getstartedkan.com/index.php">Home Page</a>
+        <a href="http://getstartedkan.com/class_presentation.php">Class Presentation</a> 
+        <a href="http://getstartedkan.com/1st_presentation.php">1st Presentation</a>
+        <a href="http://getstartedkan.com/2nd_presentation.php">2nd Presentation</a>
+        <a href="http://getstartedkan.com/3rd_presentation.php">3rd Presentation</a>
+    </body>
+</html>
---
+++ Noway/3rd_presentation.php
@@ -0,0 +1,41 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="UTF-8">
+        <title>Welcome</title>
+        <style>
+body
+{
+background-color:#FFFF99;
+}
+</style>
+    </head>
+    <body>
+        <h2>If-Else Else Statement</h2>
+        <img border="0" src="/images/if_else_else.PNG" alt="If-Else ELse" width="600" height="228">
+        <br>
+        <?php
+        
+   
+ if (5 > 6) 
+ { 
+ echo "5 is larger than 6"; 
+ } 
+ elseif (5 == 6) 
+ { 
+ echo "5 is equal to 6"; 
+ } 
+ else 
+ { 
+ echo "5 is smaller than 6"; 
+ } 
+ 
+ ?> 
+          <br>
+                  <a href="http://getstartedkan.com/index.php">Home Page</a>
+        <a href="http://getstartedkan.com/class_presentation.php">Class Presentation</a> 
+        <a href="http://getstartedkan.com/1st_presentation.php">1st Presentation</a>
+        <a href="http://getstartedkan.com/2nd_presentation.php">2nd Presentation</a>
+        <a href="http://getstartedkan.com/3rd_presentation.php">3rd Presentation</a>
+    </body>
+</html>
